f4da955415e827adeb5ca7aa75073f4a8f6d1d06
Fitrandi Ramadhan <fitrandiramadhan@gmail.com>
2017-10-29 12:23:19 +0700

    adding preparation all

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -17,7 +17,9 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  'styles/**/*.css'
+    'bower_components/bootstrap/dist/css/bootstrap.css',
+
+    'styles/**/*.css'
 ];
 
 
@@ -27,6 +29,9 @@ var jsFilesToInject = [
 
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
+
+  'bower_components/jquery/dist/jquery.js',
+  'bower_components/bootstrap/dist/js/bootstrap.js',
 
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
@@ -83,5 +88,3 @@ module.exports.templateFilesToInject = templateFilesToInject.map(function(tplPat
   }
   return require('path').join('assets/',tplPath);
 });
-
-
